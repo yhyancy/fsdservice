5ef42b4228501a87f25a70761aaa5403de6138cd
HuaYang <2397856503@qq.com>
2020-05-29 14:53:20 +0800

    iposervice - admin/ipo/list

--- iposervice/src/main/java/com/fsd/controller/AdminIpoController.java
+++ iposervice/src/main/java/com/fsd/controller/AdminIpoController.java
@@ -1,13 +1,25 @@
 package com.fsd.controller;
 
+import com.fsd.data.entity.IpoEntity;
+import com.fsd.data.mapper.IpoMapper;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
-
+@RequestMapping("admin/ipo")
 public class AdminIpoController {
 
-
-
+    private IpoMapper ipoMapper;
+    private AdminIpoController(IpoMapper ipoMapper){
+        this.ipoMapper=ipoMapper;
+    }
+    @GetMapping("list")
+    private List<IpoEntity> getIpoList(){
+        return ipoMapper.listIpo();
+    }
 }
