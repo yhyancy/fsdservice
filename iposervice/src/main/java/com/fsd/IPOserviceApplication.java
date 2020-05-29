23fed35cc5f9f7d70157898cd51c00308c66ffe6
HuaYang <2397856503@qq.com>
2020-05-29 14:40:51 +0800

    iposervice - add user/ipo/list

---
+++ iposervice/src/main/java/com/fsd/IPOserviceApplication.java
@@ -0,0 +1,11 @@
+package com.fsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPOserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IPOserviceApplication.class,args);
+    }
+}
---
+++ iposervice/src/main/java/com/fsd/controller/AdminIpoController.java
@@ -0,0 +1,13 @@
+package com.fsd.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+
+public class AdminIpoController {
+
+
+
+}
