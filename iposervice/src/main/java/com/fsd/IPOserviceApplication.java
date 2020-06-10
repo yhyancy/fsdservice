ae2032cfff5a2c1127724ea766b5b8e631338906
HuaYang <2397856503@qq.com>
2020-06-10 09:28:13 +0800

    add iposervice to eureka server

--- iposervice/src/main/java/com/fsd/IPOserviceApplication.java
+++ iposervice/src/main/java/com/fsd/IPOserviceApplication.java
@@ -2,8 +2,10 @@ package com.fsd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class IPOserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(IPOserviceApplication.class,args);
