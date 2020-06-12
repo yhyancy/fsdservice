3d9b560fa589d67b76df6d2eb3fce154f600d717
HuaYang <2397856503@qq.com>
2020-06-12 09:20:44 +0800

    companyservice - add company

---
+++ companyservice/src/main/java/com/fsd/controller/ManagecompanyController.java
@@ -0,0 +1,27 @@
+package com.fsd.controller;
+
+import com.fsd.data.entity.CompanyEntity;
+import com.fsd.data.entity.CompanyReturn;
+import com.fsd.data.mapper.CompanyMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("admin/company")
+public class ManagecompanyController {
+    private CompanyMapper companyMapper;
+    @Autowired
+    private ManagecompanyController(CompanyMapper companyMapper){
+        this.companyMapper=companyMapper;
+    }
+
+   @PostMapping("/add")
+    public CompanyReturn addCompany(@RequestBody(required = true) CompanyEntity companyEntity){
+        int status= companyMapper.addCompany(companyEntity);
+        if(status == 1){
+            return new CompanyReturn("ok");
+        }
+        return new CompanyReturn("failed");
+   }
+}
